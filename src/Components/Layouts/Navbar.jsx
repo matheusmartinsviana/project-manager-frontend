87a102d0fa4010c02c41915164424ff92b72e46f
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-07-04 17:36:24 -0300

    feat: set navbar style

--- src/Components/Layouts/Navbar.jsx
+++ src/Components/Layouts/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import style from './Styles/Navbar.module.css'
-import { FaFolder, FaUser, FaTasks, FaHome } from "react-icons/fa";
+import React from 'react';
+import { FaHome, FaTasks } from "react-icons/fa";
+import { FaUserPen, FaFilePen } from "react-icons/fa6";
+import logo from '../../assets/logoByFreepik.png';
+import style from './Styles/Navbar.module.css';
 
 export default function Navbar() {
     return (
         <div className={style.navbarContainer}>
             <picture>
-                <img src="#" alt="Project Manager Logo" />
+                <img src={logo} alt="Project Manager Logo" height={80} width={80}/>
             </picture>
             <div className={style.navbarItems}>
                 <ul className={style.item}>
-                    <li><a href="#"> <div className={style.reactIcon}><FaHome size={20} /></div>Users Operations</a></li>
+                    <li><a href="#"> <div className={style.reactIcon}><FaHome size={20} /></div>Home</a></li>
                 </ul>
                 <ul className={style.item}>
-                    <li><a href="#"> <div className={style.reactIcon}><FaUser size={20} /></div>Users Operations</a></li>
+                    <li><a href="#"> <div className={style.reactIcon}><FaUserPen size={20} /></div>Users</a></li>
                 </ul>
                 <ul className={style.item}>
-                    <li><a href="#"> <div className={style.reactIcon}><FaFolder size={20} /></div>Projects Operations</a></li>
+                    <li><a href="#"> <div className={style.reactIcon}><FaFilePen size={20} /></div>Projects</a></li>
                 </ul>
                 <ul className={style.item}>
-                    <li><a href="#"> <div className={style.reactIcon}><FaTasks size={20} /></div>Tasks Operations</a></li>
+                    <li><a href="#"> <div className={style.reactIcon}><FaTasks size={20} /></div>Tasks</a></li>
                 </ul>
             </div>
         </div>
