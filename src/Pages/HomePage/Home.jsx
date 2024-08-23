70e63b25f56086082353a92f085edeca72347cb1
matheusmartinsviana <matheusmviana@outlook.com.br>
2024-08-22 23:41:06 -0300

    feat: change wallpaper function

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,14 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import Button from '../../Components/Button'
+import React, { useState } from 'react';
+import Button from '../../Components/Button';
 import style from './Styles/Home.module.css';
 import { useNavigate } from 'react-router-dom';
 import NavHome from './NavHome';
 
 const Home = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [background, setBackground] = useState("public/4002676.jpg");
+
+    const changeWallpaper = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setBackground(e.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
     return (
-        <div className='container2'>
+        <div className="container2" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover' }}>
             <NavHome />
+            <label htmlFor="wallpaper-input">
+                <input
+                    id="wallpaper-input"
+                    type="file"
+                    name="wallpaper-changer"
+                    onChange={changeWallpaper}
+                />
+            </label>
             <div className={style.userPath}>
                 <Button onClick={() => navigate('/project-manager-frontend/user')}>Add a new User</Button>
                 <Button onClick={() => navigate('/project-manager-frontend/project')}>Add a new Project</Button>
