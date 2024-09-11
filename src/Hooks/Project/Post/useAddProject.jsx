e70ab966e95cc41d8a73177e7934743e8807ac20
Matheus Viana <matheusmviana@Outlook.com.br>
2024-09-11 20:41:15 -0300

    style: code quality with prettier

--- src/Hooks/Project/Post/useAddProject.jsx
+++ src/Hooks/Project/Post/useAddProject.jsx
@@ -1,29 +1,31 @@
 const useAddProject = () => {
-    const addProject = async (project) => {
-        try {
-            const response = await fetch("https://project-manager-74i7.onrender.com/api/v1/project", {
-                method: "POST",
-                body: JSON.stringify(project),
-                headers: {
-                    'Content-Type': 'application/json',
-                    "Authorization": `${localStorage.getItem("token")}`
-                }
-            });
-
-            if (!response.ok) {
-                const errorMessage = await response.json()
-                throw new Error(errorMessage.error);
-            }
-
-            return await response.json();
-
-        } catch (error) {
-            console.error('Error adding project:', error);
-            return { error: error.message };
+  const addProject = async (project) => {
+    try {
+      const response = await fetch(
+        "https://project-manager-74i7.onrender.com/api/v1/project",
+        {
+          method: "POST",
+          body: JSON.stringify(project),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("token")}`,
+          },
         }
-    };
+      );
 
-    return { addProject };
+      if (!response.ok) {
+        const errorMessage = await response.json();
+        throw new Error(errorMessage.error);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Error adding project:", error);
+      return { error: error.message };
+    }
+  };
+
+  return { addProject };
 };
 
 export default useAddProject;
--- src/Hooks/Project/Put/useUpdateProject.jsx
+++ src/Hooks/Project/Put/useUpdateProject.jsx
@@ -1,29 +1,31 @@
 const useUpdateProject = () => {
-    const updateProject = async (project, id) => {
-        try {
-            const response = await fetch(`https://project-manager-74i7.onrender.com/api/v1/project/${id}`, {
-                method: "PUT",
-                body: JSON.stringify(project),
-                headers: {
-                    'Content-Type': 'application/json',
-                    "Authorization": `${localStorage.getItem("token")}`
-                }
-            });
-
-            if (!response.ok) {
-                const errorMessage = await response.json()
-                throw new Error(errorMessage.error);
-            }
-
-            return await response.json();
-
-        } catch (error) {
-            console.error('Error updating project:', error);
-            return { error: error.message };
+  const updateProject = async (project, id) => {
+    try {
+      const response = await fetch(
+        `https://project-manager-74i7.onrender.com/api/v1/project/${id}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(project),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("token")}`,
+          },
         }
-    };
+      );
 
-    return { updateProject };
+      if (!response.ok) {
+        const errorMessage = await response.json();
+        throw new Error(errorMessage.error);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Error updating project:", error);
+      return { error: error.message };
+    }
+  };
+
+  return { updateProject };
 };
 
 export default useUpdateProject;
