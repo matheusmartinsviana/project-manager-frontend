a815fd7a34ced083eb4696ce9aa00b8e56c9ca08
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-09-17 19:36:44 -0300

    refact: navbar

--- src/Components/Entities/Project/Forms/FormAddProject.jsx
+++ src/Components/Entities/Project/Forms/FormAddProject.jsx
@@ -1,15 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import useAddProject from "../../../../Hooks/Project/Post/useAddProject.jsx";
 import Form from "../../../General/Form";
 import useGetProjectsData from "../../../../Hooks/Project/Get/UseGetProjectsData.jsx";
 import Input from "../../../General/Input";
-import Select from "../../../General/Select.jsx";
-import useGetUsersData from "../../../../Hooks/User/Get/useGetUsersData.jsx";
 
+// eslint-disable-next-line react/prop-types
 const FormAddProject = ({ onUserAction }) => {
   const { addProject } = useAddProject();
   const { fetchProjects } = useGetProjectsData();
-  const { users, loading: usersLoading, error: usersError } = useGetUsersData(); // Handle loading and error in the hook
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
@@ -18,18 +16,6 @@ const FormAddProject = ({ onUserAction }) => {
     description: "",
     userId: "",
   });
-
-  useEffect(() => {
-    // Ensure users data is loaded before rendering
-  }, [users]);
-
-  const handleUserSelect = (e) => {
-    const selectedUserId = e.target.value;
-    setProjectData({
-      ...projectData,
-      userId: selectedUserId,
-    });
-  };
 
   const handleChange = (e) => {
     setProjectData({
@@ -89,23 +75,6 @@ const FormAddProject = ({ onUserAction }) => {
         required
         autoComplete="off"
       />
-      {/* <label htmlFor="user-select">Choose a user for this project:</label>
-      <Select
-        id="user-select"
-        value={projectData.userId}
-        onChange={handleUserSelect}
-        required
-      >
-        <option value="" disabled>
-          {usersLoading ? "Loading users..." : "Select a user"}
-        </option>
-        {!usersLoading && !usersError && users.map((user) => (
-          <option key={user.id} value={user.id}>
-            {user.email}
-          </option>
-        ))}
-      </Select> */}
-      {usersError && <p className="error-message">{usersError}</p>}
       {error && <p className="error-message">{error}</p>}
       {success && <p className="success-message">{success}</p>}
       <button id="submit-button" type="submit" disabled={isSubmitting}>
